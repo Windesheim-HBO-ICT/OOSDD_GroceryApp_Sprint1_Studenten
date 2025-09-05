81ca58d9a42b006c9827ad91b3348f77b690905e
Wout van Gurp <wout@brightflow.nl>
2025-09-05 14:06:08 +0200

    boodschappen toegevoegd

--- Grocery.Core.Data/Repositories/GroceryListRepository.cs
+++ Grocery.Core.Data/Repositories/GroceryListRepository.cs
@@ -12,7 +12,8 @@ namespace Grocery.Core.Data.Repositories
             groceryLists = [
                 new GroceryList(1, "Boodschappen familieweekend", DateOnly.Parse("2024-12-14"), "#FF6A00", 1),
                 new GroceryList(2, "Kerstboodschappen", DateOnly.Parse("2024-12-07"), "#626262", 1),
-                new GroceryList(3, "Weekend boodschappen", DateOnly.Parse("2024-11-30"), "#003300", 1)];
+                new GroceryList(3, "doordeweekse boodschappen", DateOnly.Parse("2025-9-05"), "# 0728ff", 1),
+                new GroceryList(4, "Weekend boodschappen", DateOnly.Parse("2024-11-30"), "#003300", 1)];
         }
 
         public List<GroceryList> GetAll()
