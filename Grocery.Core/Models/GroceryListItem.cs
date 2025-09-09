3c7c6123dc327e2238c126e287bd0b23f0f39c21
JammingEnd <57032230+JammingEnd@users.noreply.github.com>
2025-09-09 12:21:44 +0200

    GroceryListUpdate
    Changed fields to properties with private sets,
    completed the constructor

--- Grocery.Core/Models/GroceryListItem.cs
+++ Grocery.Core/Models/GroceryListItem.cs
@@ -1,13 +1,18 @@
 ﻿namespace Grocery.Core.Models
 {
+ 
     public class GroceryListItem : Model
     {
-        public int GroceryListId; //To do: change it to a property
-        public int ProductId;     //To do: change it to a property
-        public int Amount { get; set; }
+        // Properties set private since we dont want to change them freely. constructor only
+        public int GroceryListId { get; private set; }
+        public int ProductId { get; private set; }
+        public int Amount { get; private set; }
         public GroceryListItem(int id, int groceryListId, int productId, int amount) : base(id, "")
         {
-            //To do:assign values to the properties
+            GroceryListId = groceryListId;
+            ProductId = productId;
+            Amount = amount;
+
         }
 
         public Product Product { get; set; } = new(0, "None", 0);
