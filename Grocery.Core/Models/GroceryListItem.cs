50abcc5b69383890080f2f0aa5a379cf6bddd0ae
esnerv <s1182106@student.windesheim.nl>
2025-09-09 11:26:51 +0200

    UC 2 uitgewerkt

--- Grocery.Core/Models/GroceryListItem.cs
+++ Grocery.Core/Models/GroceryListItem.cs
@@ -2,12 +2,15 @@
 {
     public class GroceryListItem : Model
     {
-        public int GroceryListId; //To do: change it to a property
-        public int ProductId;     //To do: change it to a property
+        public int GroceryListId { get; set; } //Done: change it to a property
+        public int ProductId { get; set; }    //Done: change it to a property
         public int Amount { get; set; }
         public GroceryListItem(int id, int groceryListId, int productId, int amount) : base(id, "")
         {
-            //To do:assign values to the properties
+            this.GroceryListId = groceryListId;
+            this.ProductId = productId;
+            this.Amount = amount;
+            //Done:assign values to the properties
         }
 
         public Product Product { get; set; } = new(0, "None", 0);
