006044d0945c7470778d92f7f07326a00c9aee87
SdB2005 <semdeboerdere@gmail.com>
2025-09-10 13:32:53 +0200

    added products to product list

--- Grocery.Core.Data/Repositories/ProductRepository.cs
+++ Grocery.Core.Data/Repositories/ProductRepository.cs
@@ -9,13 +9,10 @@ namespace Grocery.Core.Data.Repositories
         public ProductRepository()
         {
             products = [
-                /* 
-                 * initieer de lijst met 4 nieuwe producten: 
-                 * Melk[voorraad 300], 
-                 * Kaas[voorraad 100], 
-                 * Brood[voorraad 400] en 
-                 * Cornflakes[voorraad 0]
-                */
+                new Product(1, "Melk", 300),
+                new Product(2, "Kaas", 100),
+                new Product(3, "Brood", 400),
+                new Product(4, "Cornflakes", 0)
 				];
         }
         public List<Product> GetAll()
