e9438f2a6a36730c663c41b9cdc2da03631ab1c1
JammingEnd <57032230+JammingEnd@users.noreply.github.com>
2025-09-10 09:05:11 +0200

    Build the repository
    - added new items
    - filled in methods with appropriate return type

--- Grocery.Core.Data/Repositories/ProductRepository.cs
+++ Grocery.Core.Data/Repositories/ProductRepository.cs
@@ -9,18 +9,15 @@ namespace Grocery.Core.Data.Repositories
         public ProductRepository()
         {
             products = [
-                /* 
-                 * initieer de lijst met 4 nieuwe producten: 
-                 * Melk[voorraad 300], 
-                 * Kaas[voorraad 100], 
-                 * Brood[voorraad 400] en 
-                 * Cornflakes[voorraad 0]
-                */
-				];
+                new Product(0, "Melk", 300),
+                new Product(1, "Kaas", 100), 
+                new Product(2, "Brood", 100), 
+                new Product(3, "Cornflakes", 100),
+                ];
         }
         public List<Product> GetAll()
         {
-            return []; //wijzig dit in de gemaakte lijst uit de constructo
+            return products;
         }
 
         public Product? Get(int id)
@@ -30,17 +27,30 @@ namespace Grocery.Core.Data.Repositories
 
         public Product Add(Product item)
         {
-            throw new NotImplementedException();
+            if(item == null)
+            {
+                return null;
+            }
+            products.Add(item);
+            return item;
         }
 
         public Product? Delete(Product item)
         {
-            throw new NotImplementedException();
+            Product inst = products.Where(x => x.Id == item.Id).First();
+            products.Remove(inst);
+            return item;
         }
 
         public Product? Update(Product item)
         {
-            throw new NotImplementedException();
+            Product inst = products.Where(x => x.Id == item.Id).First();
+            if (inst != null)
+            {
+                inst = item;
+                return inst;
+            }
+            return null;
         }
     }
 }
