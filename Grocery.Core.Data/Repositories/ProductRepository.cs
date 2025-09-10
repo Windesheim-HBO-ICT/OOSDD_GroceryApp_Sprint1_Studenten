e1ec9d2174a0cb3d287ec266e5c71557d40dffaf
SdB2005 <semdeboerdere@gmail.com>
2025-09-10 13:34:05 +0200

    fixed product list by returning products

--- Grocery.Core.Data/Repositories/ProductRepository.cs
+++ Grocery.Core.Data/Repositories/ProductRepository.cs
@@ -17,7 +17,7 @@ namespace Grocery.Core.Data.Repositories
         }
         public List<Product> GetAll()
         {
-            return []; //wijzig dit in de gemaakte lijst uit de constructo
+            return products;
         }
 
         public Product? Get(int id)
