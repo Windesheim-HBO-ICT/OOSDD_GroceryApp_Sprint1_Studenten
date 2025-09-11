8dc4f13da5cf11387f84f7a9d6f8a0d8927a5588
Anton <s1211894@student.windesheim.nl>
2025-09-11 18:09:17 +0200

    put down a couple wrong ; and discovered this while trying to test

--- Grocery.Core/Models/GroceryListItem.cs
+++ Grocery.Core/Models/GroceryListItem.cs
@@ -5,12 +5,12 @@
 
         public int GroceryListId { get; set; }
         public int ProductId { get; set; }
-        public int Amount { get; set; };
+        public int Amount { get; set; }
         public GroceryListItem(int id, int groceryListId, int productId, int amount) : base(id, "")
         {
-           GroceryListId = groceryListId;
-           ProductId = productId;
-           Amount = amount;
+            GroceryListId = groceryListId;
+            ProductId = productId;
+            Amount = amount;
         }
 
         public Product Product { get; set; } = new(0, "None", 0);
