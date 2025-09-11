42572c84c7efb63bffe68b7add39feaa974d2832
Carlos <79849894+6Waifu9@users.noreply.github.com>
2025-09-11 14:54:49 +0200

    GroceryListItem: velden vervangen door properties

--- Grocery.Core/Models/GroceryListItem.cs
+++ Grocery.Core/Models/GroceryListItem.cs
@@ -2,12 +2,17 @@
 {
     public class GroceryListItem : Model
     {
-        public int GroceryListId; //To do: change it to a property
-        public int ProductId;     //To do: change it to a property
+        // Changed from fields to properties
+        public int GroceryListId { get; set; }
+        public int ProductId { get; set; }
         public int Amount { get; set; }
+
+        // Constructor assigns values to properties
         public GroceryListItem(int id, int groceryListId, int productId, int amount) : base(id, "")
         {
-            //To do:assign values to the properties
+            GroceryListId = groceryListId;
+            ProductId = productId;
+            Amount = amount;
         }
 
         public Product Product { get; set; } = new(0, "None", 0);
