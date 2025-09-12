df01d9a1bdec7fc02b151dcfce20c51ca34f9f7f
JDAJohnsen <s1219142@student.windesheim.nl>
2025-09-12 20:59:14 +0200

    Verandering uc 1 t/m 3
    uc1 t/m 3

--- Grocery.Core/Models/GroceryListItem.cs
+++ Grocery.Core/Models/GroceryListItem.cs
@@ -2,14 +2,21 @@
 {
     public class GroceryListItem : Model
     {
-        public int GroceryListId; //To do: change it to a property
-        public int ProductId;     //To do: change it to a property
+        // Eigenschappen i.p.v. velden
+        public int GroceryListId { get; set; }
+        public int ProductId { get; set; }
         public int Amount { get; set; }
-        public GroceryListItem(int id, int groceryListId, int productId, int amount) : base(id, "")
-        {
-            //To do:assign values to the properties
-        }
 
+        // Navigation property
         public Product Product { get; set; } = new(0, "None", 0);
+
+        // Constructor
+        public GroceryListItem(int id, int groceryListId, int productId, int amount)
+            : base(id, "")
+        {
+            GroceryListId = groceryListId;
+            ProductId = productId;
+            Amount = amount;
+        }
     }
 }
