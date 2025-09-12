e7abd55d76528fc6b70a2fbbf2a7697728d6fae0
SanneAbrahamse <sanne.abrahamse@windesheim.nl>
2025-09-12 13:22:49 +0200

    UC1 t/m 3

--- Grocery.Core.Data/Repositories/ProductRepository.cs
+++ Grocery.Core.Data/Repositories/ProductRepository.cs
@@ -8,19 +8,18 @@ namespace Grocery.Core.Data.Repositories
         private readonly List<Product> products;
         public ProductRepository()
         {
-            products = [
-                /* 
-                 * initieer de lijst met 4 nieuwe producten: 
-                 * Melk[voorraad 300], 
-                 * Kaas[voorraad 100], 
-                 * Brood[voorraad 400] en 
-                 * Cornflakes[voorraad 0]
-                */
-				];
+            products = new List<Product>();
+            {
+                new Product(0, "Melk", 300);
+                new Product(1, "Kaas", 100);
+                new Product(2, "Brood", 400);
+                new Product(3, "Cornflakes", 0);
+            }
         }
+        
         public List<Product> GetAll()
         {
-            return []; //wijzig dit in de gemaakte lijst uit de constructo
+            return products;
         }
 
         public Product? Get(int id)
