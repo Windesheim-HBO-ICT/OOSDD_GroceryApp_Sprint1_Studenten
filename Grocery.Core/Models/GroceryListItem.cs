1eda7f31adfcfc734578897811264dba24af8981
Jasper125 <107043641+Jasper125@users.noreply.github.com>
2025-09-12 09:38:50 +0200

    UC 1, 2 en 3
    UC 1, 2 en 3.
    - UC1: Tonen boodschappenlijsten werkend.
    - UC2: Tonen inhoud boodschappenlijsten werkend.
    - UC3: Tonen producten werkend.

--- Grocery.Core/Models/GroceryListItem.cs
+++ Grocery.Core/Models/GroceryListItem.cs
@@ -2,12 +2,16 @@
 {
     public class GroceryListItem : Model
     {
-        public int GroceryListId; //To do: change it to a property
-        public int ProductId;     //To do: change it to a property
+        public int GroceryListId { get; set; } //To do: change it to a property
+        public int ProductId { get; set; }     //To do: change it to a property
         public int Amount { get; set; }
         public GroceryListItem(int id, int groceryListId, int productId, int amount) : base(id, "")
         {
             //To do:assign values to the properties
+            this.Id = id;
+            this.GroceryListId = groceryListId;
+            this.ProductId = productId;
+            this.Amount = amount;
         }
 
         public Product Product { get; set; } = new(0, "None", 0);
