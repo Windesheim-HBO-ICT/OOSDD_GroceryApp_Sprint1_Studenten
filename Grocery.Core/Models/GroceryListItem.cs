301c4fa28fb3af8f6f66b0bb6a7bfa4297158e1d
DanialBillows <danialbillows@icloud.com>
2025-09-12 17:31:30 +0200

    values assigned

--- Grocery.Core/Models/GroceryListItem.cs
+++ Grocery.Core/Models/GroceryListItem.cs
@@ -7,7 +7,10 @@
         public int Amount { get; set; }
         public GroceryListItem(int id, int groceryListId, int productId, int amount) : base(id, "")
         {
-            //To do:assign values to the properties
+            Id = id;
+            GroceryListId = groceryListId;
+            ProductId = productId;
+            Amount = amount;                //To do:assign values to the properties. Done
         }
 
         public Product Product { get; set; } = new(0, "None", 0);
